baeeb555369f8fb44f7ee9627170517dfd3b72d8
Sathyanarayanan Saravanamuthu <sathya.tvr.cbe@gmail.com>
2019-09-04 14:06:22 +0530

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -58,8 +58,7 @@ dag = DAG(
     'tutorial',
     default_args=default_args,
     description='A simple tutorial DAG',
-    schedule_interval=timedelta(days=1),
-    image='ubuntu:latest'
+    schedule_interval=timedelta(days=1)   
 )
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
